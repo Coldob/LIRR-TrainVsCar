8f5bf256dbc08dc855b4cf961c182c85afe4b7f6
Coldob <101027929+Coldob@users.noreply.github.com>
2022-08-23 01:20:16 -0400

    Update TvCGraphCalculator.py
    I added a graph code can be cleaned up especially since I used floats way more than strings and kept converting them also only station is Seaford in the XML still

--- TvCGraphCalculator.py
+++ TvCGraphCalculator.py
@@ -1,5 +1,8 @@
 from xml.dom import minidom
 import math
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+import numpy as np
 
 mpg = 'unanswered'
 cpg = 'unanswered'
@@ -7,11 +10,14 @@ distance = 'unanswered'
 hStation = 'unanswered'
 yTicket = 'unanswered'
 daysTaken = 'unanswered'
-file = minidom.parse('TrainPrice.xml');
-stations = file.getElementsByTagName('station');
+file = minidom.parse('TrainPrice.xml')
+stations = file.getElementsByTagName('station')
 y = "equation"
 tickets = ['peak', 'offpeak', 'seniorCitDisMed', 'cityticket', 'oneWayAtlantic', 'weeklyAtlantic', 'monthly', 'weekly', 'tenTripPeak', 'offPeakTenTrip', 'tenTripSeniorDisMed', 'twentyTripPeak', 'mil']
-
+xAxis = [1,2,3,4,5,6,7,8,9,10,11,12]
+tyAxis = []
+cyAxis = []
+fig, ax = plt.subplots()
 
 def findStation(station):
     i = 0
@@ -32,7 +38,7 @@ def ticketCost(tickettype):
 
 def tCostPerMonth(month):
     if yTicket == 'monthly':
-        y = month*ticketCost(yTicket)
+        y = month*cTicket
         sCost = str(y)
         return(sCost)
     if yTicket == 'peak':
@@ -115,7 +121,7 @@ def cCostPerMonth(month):
 
 #maybe correct center to centre
 while mpg == 'unanswered':
-    mpg = input("What is your average MPG:")
+    mpg = input("What is your cars average city MPG:")
     try:
         float(mpg)
         print(mpg)
@@ -124,7 +130,7 @@ while mpg == 'unanswered':
         mpg = 'unanswered'
         print("please enter a valid number")
 while cpg == 'unanswered':
-    cpg = input("How much do you pay per Gallon:")
+    cpg = input("How much do you typically pay per Gallon:")
     try:
         float(cpg)
         print(cpg)
@@ -133,7 +139,7 @@ while cpg == 'unanswered':
         cpg = 'unanswered'
         print("please enter a valid number")
 while distance == 'unanswered':
-    distance = input("How far is to the city by car in miles:")
+    distance = input("How far is it to drive by car to manhattan in miles:")
     try:
         float(distance)
         print(distance)
@@ -142,7 +148,7 @@ while distance == 'unanswered':
         distance = 'unanswered'
         print("please enter a valid number")
 while hStation == 'unanswered':
-    hStation = input("What is your normal station:")
+    hStation = input("What station are you using:")
     hStation = hStation.lower()
     if " " in hStation:
         print("spaces")
@@ -157,7 +163,7 @@ while hStation == 'unanswered':
         i = findStation(hStation)
         print(hStation)
 while yTicket == 'unanswered':
-    yTicket = input("What ticket do you normally use:")
+    yTicket = input("What ticket do you normally use (press enter for spelling and options):")
     try:
         inlist = str(tickets.index(yTicket))
         print(ticketCost(yTicket))
@@ -174,7 +180,54 @@ while daysTaken == 'unanswered':
         daysTaken = int(daysTaken)
         print(daysTaken)
 
-print("this is the cost per month:$"+tCostPerMonth(1))
-print("this is cost for car:$"+cCostPerMonth(1))
-print("this is the cost per year:$"+tCostPerMonth(12))
-print("this is cost for car:$"+cCostPerMonth(12))
+print("this is the cost per month for the train:$"+tCostPerMonth(1))
+print("this is cost per month for driving:$"+cCostPerMonth(1))
+print("this is the cost per year for the train:$"+tCostPerMonth(12))
+print("this is cost per year for driving:$"+cCostPerMonth(12))
+
+if float(tCostPerMonth(12)) > float(cCostPerMonth(12)):
+    toplimit = float(tCostPerMonth(12)) + 50
+    plt.ylim(0,toplimit)
+    print("train")
+else:
+    toplimit = float(cCostPerMonth(12)) + 50
+    plt.ylim(0,toplimit)
+    print("car")
+
+def yaxis(vehicle):
+    x=0
+    if vehicle == "train":
+        for x in range(12):
+            result = float(tCostPerMonth(x))
+            tyAxis.append(result)
+    else:
+        for x in range(12):
+            result = float(cCostPerMonth(x))
+            cyAxis.append(result)
+yaxis("train")
+yaxis("car")
+
+print(cyAxis)
+print(tyAxis)
+
+plt.step(xAxis, tyAxis, label='Train')
+plt.step(xAxis, cyAxis, label='Car')
+
+ax.legend(loc='lower right')
+
+plt.xlim(1,12)
+
+plt.xticks(np.arange(1, 12, 1))
+plt.yticks(np.arange(0, toplimit, 300.00))
+
+ax.yaxis.set_minor_locator(MultipleLocator(100.00))
+
+ax.grid(True, linestyle='-.')
+
+plt.xlabel('Month')
+plt.ylabel('Cost in USD')
+
+ax.yaxis.set_major_formatter('${x:1.2f}')
+
+plt.title('The estimated cost to drive to Manhattan compared to taking the LIRR to Pennstation (*not including parking*)')
+plt.show()
